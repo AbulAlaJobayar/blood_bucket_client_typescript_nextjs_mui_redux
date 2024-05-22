0832b97d821941364d6a1531421210c23efe09ad
Abul Ala Jobayar <jobayar59@gmail.com>
2024-05-22 10:22:26 +0600

    create reusable input form

--- src/components/Form/BBInput.tsx
+++ src/components/Form/BBInput.tsx
@@ -1,8 +1,45 @@
-const BBInput = () => {
+import { SxProps, TextField } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
+type TInputProps = {
+  size?: "small" | "medium";
+  type?: string;
+  name: string;
+  label?: string;
+  fullWidth?: boolean;
+  required?: boolean;
+  sx?: SxProps;
+};
+
+const BBInput = ({
+  size = "small",
+  type = "text",
+  name,
+  label,
+  fullWidth,
+  required,
+  sx,
+}: TInputProps) => {
+  const { control } = useFormContext();
   return (
-    <div>
-      <h1>This is BBInput component</h1>
-    </div>
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          sx={{ ...sx }}
+          label={label}
+          variant="outlined"
+          type={type}
+          size={size}
+          fullWidth={fullWidth}
+          required={required}
+          placeholder={label}
+          error={!!error?.message}
+          helperText={error?.message}
+        />
+      )}
+    />
   );
 };
 
