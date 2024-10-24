6b34d6d5dd90d63065940eee194fee7bf70bcbcf
AbulAlaJobayar <jobayar59@gmail.com>
2024-10-24 15:01:11 +0600

    update login credential

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -1,6 +1,20 @@
 "use client";
 
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  Typography,
+} from "@mui/material";
 import BBForm from "@/components/Form/BBForm";
 import BBInput from "@/components/Form/BBInput";
 import Link from "next/link";
@@ -10,7 +24,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import GoogleIcon from "@mui/icons-material/Google";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { userLogin } from "@/services/action/userLogin";
 import { storeUserInfo } from "@/services/authService";
 import { toast } from "sonner";
@@ -24,7 +38,26 @@ const validationSchema = z.object({
 
 const LoginPage = () => {
   const [error, setError] = useState();
+  const [role, setRole] = useState("user");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // set Role
+  const handleRoleChange = (e: SelectChangeEvent<string>) => {
+    console.log(e.target.value);
+    const selectedRole = e.target.value;
+    setRole(selectedRole);
+  };
+  useEffect(() => {
+    if (role === "user") {
+      setEmail("user@gmail.com");
+      setPassword("user123456");
+    } else {
+      setEmail("admin@gmail.com");
+      setPassword("admin123456");
+    }
+  }, [role]);
 
+  // Handle login
   const handleLogin = async (values: FieldValues) => {
     console.log(values);
     try {
@@ -40,6 +73,11 @@ const LoginPage = () => {
     } catch (error: any) {
       console.log(error.message);
     }
+  };
+  // set default value
+  const defaultValues = {
+    email: "",
+    password: "",
   };
   return (
     <Box>
@@ -64,6 +102,7 @@ const LoginPage = () => {
           justifyContent={"space-between"}
           alignItems={"center"}
         >
+          {/* login Credential */}
           <Box sx={{ flex: 1 }}>
             <Box>
               <Typography
@@ -76,28 +115,78 @@ const LoginPage = () => {
               <Typography variant="h4" fontWeight={700}>
                 Credential
               </Typography>
-             <Box mt={5} maxWidth={'500px'}>
-             <Marquee >
-                ...... Login Credential:user
-                (Email:user@gmail.com,password:user123456); admin:
-                (email:admin@gmail.com,password:admin123456)....
-              </Marquee>
+              <Box mt={5} maxWidth={"500px"}>
+                <FormControl fullWidth margin="normal">
+                  <Select value={role} onChange={handleRoleChange}>
+                    <MenuItem value="user">User</MenuItem>
+                    <MenuItem value="admin">Admin</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              {role && (
+                <>
+                  <Card
+                    variant="outlined"
+                    sx={{ maxWidth: "500px", margin: "20px 0" }}
+                  >
+                    <CardContent>
+                      <Typography variant="h5" component="div" gutterBottom>
+                        Credentials
+                      </Typography>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Box
+                          mb={2}
+                          sx={{
+                            display: "flex",
+                            alignItems:"center",
+                            justifyContent:'center',
+                            gap: 2,
+                            backgroundColor: "#EB2C29",
+                            borderRadius: "3px",
+                            padding:"2px 8px",
+                            boxShadow: 0
 
-             </Box>
-              {/* <Stack direction={"row"} spacing={2} my={2}>
-                <Button disabled
-                  onClick={() =>
-                    signIn("github", {
-                      callbackUrl: "http://localhost:3000/dashboard",
-                    })
-                  }
-                >
-                  <GitHubIcon />
-                </Button>
-                <Button disabled onClick={() => signIn("google")}>
-                  <GoogleIcon />
-                </Button>
-              </Stack> */}
+                          }}
+                        >
+                          <Typography  color="white" fontWeight='500'>
+                            Email:
+                          </Typography>
+                          <Typography  sx={{ color: "white" }} fontWeight='500'>
+                            {email}
+                          </Typography>
+                        </Box>
+                        <Box
+                          mb={2}
+                          sx={{
+                            display: "flex",
+                            alignItems:"center",
+                            justifyContent:'center',
+                            gap: 2,
+                            backgroundColor: "#EB2C29",
+                            borderRadius: "3px",
+                            padding:"2px 8px",
+                            boxShadow: 0
+
+                          }}
+                        >
+                          <Typography  color="white" fontWeight='500'>
+                            Password:
+                          </Typography>
+                          <Typography  sx={{ color: "white" }} fontWeight='500'>
+                            {password}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </>
+              )}
+              
             </Box>
           </Box>
           <Box sx={{ flex: 1 }}>
@@ -109,7 +198,7 @@ const LoginPage = () => {
               <BBForm
                 onSubmit={handleLogin}
                 resolver={zodResolver(validationSchema)}
-                defaultValues={{ email: "", password: "" }}
+                defaultValues={defaultValues}
               >
                 <Grid container spacing={3} my={1}>
                   <Grid item md={12}>
